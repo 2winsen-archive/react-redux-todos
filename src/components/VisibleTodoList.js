50f6795e73e1a274949170d9b60dc9a2330b4fe7
2winsen <vitalijs.shakels@gmail.com>
2018-09-04 19:49:55 +0300

    Redux: Colocating Selectors with Reducers

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,22 +2,10 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
-
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'active':
-      return todos.filter(t => !t.completed);
-    case 'completed':
-      return todos.filter(t => t.completed);
-    default:
-      return todos;
-  }
-};
+import { getVisibleTodos } from '../reducers';
 
 const mapStateToTodoListProps = (state, { match }) => ({
-  todos: getVisibleTodos(state.todos, match.params.filter || 'all')
+  todos: getVisibleTodos(state, match.params.filter || 'all')
 });
 
 export default withRouter(
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -12,12 +12,25 @@ const todo = (state = {}, action) => {
   }
 };
 
-export const todos = (state = [], action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [...state, todo(undefined, action)];
     case 'TOGGLE_TODO':
       return state.map(t => todo(t, action));
+    default:
+      return state;
+  }
+};
+
+export const getVisibleTodos = (state, filter) => {
+  switch (filter) {
+    case 'all':
+      return state;
+    case 'active':
+      return state.filter(t => !t.completed);
+    case 'completed':
+      return state.filter(t => t.completed);
     default:
       return state;
   }
