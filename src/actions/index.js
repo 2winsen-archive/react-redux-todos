f0580f473eee80cb85aef81fd44b51736c50780d
2winsen <vitalijs.shakels@gmail.com>
2018-09-16 21:57:13 +0300

    Redux: Normalizing API Responses with normalizr

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
-import { v4 } from 'node-uuid';
 import * as api from '../api';
+import { normalize } from 'normalizr';
+import * as schema from '../schema';
 import { getIsFetching } from '../reducers/index';
 
 export const fetchTodos = filter => (dispatch, getState) => {
@@ -16,7 +17,7 @@ export const fetchTodos = filter => (dispatch, getState) => {
         dispatch({
           type: 'FETCH_TODOS_SUCCESS',
           filter,
-          response
+          response: normalize(response, schema.arrayOfTodos)
         });
       },
       error => {
@@ -29,12 +30,13 @@ export const fetchTodos = filter => (dispatch, getState) => {
 }
 
 export const addTodo = text => dispatch => {
-  return api.addTodo(text).then(response => {
-    dispatch({
-      type: 'ADD_TODO_SUCCESS',
-      response
+  return api.addTodo(text)
+    .then(response => {
+      dispatch({
+        type: 'ADD_TODO_SUCCESS',
+        response: normalize(response, schema.todo)
+      })
     })
-  })
 };
 
 export const toggleTodo = id => ({
